da58fdbb8c79039a6f31d71560d2a652be1a6f3b
kunal-khakal <khakalkunal3@gmail.com>
2026-06-30 19:02:45 +0530

    style_background_home,style_background_dashboard

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from src.screens.home_screen import home_screen
 from src.screens.teacher_screen import teacher_screen
-from src.screens.student_screen import student_screen
+from src.screens.stundent_screen import student_screen
 
 
 def main():
---
+++ src/ui/base_layout.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+
+
+def style_background_home():
+
+    st.markdown("""
+        <style>
+
+                .stApp {
+                    background: #5865F2 !important;
+                }
+
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                    }
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
+
+def style_background_dashboard():
+
+    st.markdown("""
+        <style>
+
+                .stApp {
+                    background: #E0E3FF !important;
+                }
+
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
+
+    
