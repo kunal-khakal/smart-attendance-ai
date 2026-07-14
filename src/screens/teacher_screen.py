8ea73a54caf8078aa3b390fc136127af16d9d9e8
kunal-khakal <khakalkunal3@gmail.com>
2026-07-14 10:46:09 +0530

    teacher_tab_take_attendance_1

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -250,3 +250,31 @@ def teacher_tab_manage_subjects():
         st.info("NO SUBJECTS FOUND. CREATE ONE ABOVE")
 
 
+def teacher_tab_take_attendance():
+    teacher_id = st.session_state.teacher_data['teacher_id']
+    st.header('Take AI Attendance')
+
+
+    if 'attendance_images' not in st.session_state:
+        st.session_state.attendance_images = []
+
+    subjects = get_teacher_subjects(teacher_id)
+
+    if not subjects:
+        st.warning('You havent created any subjects yet! Please create one to begin!')
+        return
+    
+    subject_options = {f"{s['name']} - {s['subject_code']}": s['subject_id'] for s in subjects}
+
+    col1, col2 = st.columns([3,1], vertical_alignment='bottom')
+
+    with col1:
+        selected_subject_label = st.selectbox('Select Subject', options=list(subject_options.keys()))
+
+    with col2:
+        if st.button('Add Photos', type='primary', icon=':material/photo_prints:', width='stretch'):
+            add_photos_dialog()
+
+    selected_subject_id = subject_options[selected_subject_label]
+
+    st.divider()
